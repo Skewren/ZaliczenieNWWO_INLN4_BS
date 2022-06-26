346dde25e1f457b61779897101b755c22841e4f7
Psychotermic <100023450+Psychotermic@users.noreply.github.com>
2022-06-26 12:30:38 +0200

    Create camera_control.cs

---
+++ Assets/camera_control.cs
@@ -0,0 +1,28 @@
+public class camera_control : MonoBehaviour
+{
+	public float MinX;
+	public float MaxX;
+	public float MinY;
+	public float MaxY;
+	public float Offset;
+	public float Distance;
+	private GameObject Player;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+    Player=GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    float posX=Player.transform.position.x;
+	float posY=Player.transform.position.y;
+
+	posX=Mathf.Clamp(posX, MinX, MaxX);
+    posY=Mathf.Clamp(posY, MinY, MaxY)+Offset;
+	
+	transform.position=new Vector3(posX,posY,Distance);
+    }
+}
