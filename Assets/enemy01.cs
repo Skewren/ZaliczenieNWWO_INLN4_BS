c71148ba3f20c0240793227db719db7f01bc5627
Psychotermic <100023450+Psychotermic@users.noreply.github.com>
2022-06-26 12:35:21 +0200

    Create enemy01.cs

---
+++ Assets/enemy01.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy01 : MonoBehaviour
+{
+	public float speed;
+	private int direction=1;
+	private Rigidbody rb;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+    rb=GetComponent<Rigidbody>();  
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+	RaycastHit wall;
+	Debug.DrawRay(transform.position,new Vector3(direction,0,0), Color.blue, 0.1f);
+	if (Physics.Raycast(transform.position,new Vector3(direction,0,0), out wall, 0.45f, 1, QueryTriggerInteraction.Ignore))
+		{
+		if (wall.collider.tag!="Player")
+			{
+			direction--direction;	
+			}
+		}
+	
+    float move=speed*direction;
+	rb.velocity=new Vector3(move,rb.velocity.y,0);	
+    }
+	
+	void OnCollisionEnter(Collision hit)
+	{
+	if (hit.gameObject.compareTag("Player"))
+		{
+		hit.gameObject.GetComponent<gamemechanics>().Dead=true;
+		}	
+	}
+	
+}
